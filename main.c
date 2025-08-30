5129bb167aafe5c06cfd0931b1efb4c8db75605c
Leon Dick <ldick@3-F-4.42heilbronn.de>
2025-08-30 14:22:02 +0200

    started

---
+++ main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldick <ldick@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/30 13:17:08 by ldick             #+#    #+#             */
+/*   Updated: 2025/08/30 13:27:42 by ldick            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include ".h"
+
+int main(int argc, char **argv)
+{
+	
+}
+
+
+//TODO change makefile to create library and not exeeecutable
+
+/*TODO Roadmap
+	create tests for functionws, 
+	
+*/
